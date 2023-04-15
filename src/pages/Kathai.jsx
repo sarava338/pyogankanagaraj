285a09674392c1ce4a7a244acc01c62215e4aab5
sarava338 <sarava338@gmail.com>
2023-04-16 03:45:04 +0530

    Add: Added firebase database

--- src/pages/Kathai.jsx
+++ src/pages/Kathai.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../components/post/Post";
+import { getPosts } from "../firebase/db";
+import { collection } from "firebase/firestore";
+import { db } from "../../fireebase";
 
 const Kathai = () => {
+  const [kathaigal, setKathaigal] = useState([]);
+
+  const kathaiRef = collection(db, "kathai");
+
+  useEffect(() => {
+    getPosts(kathaiRef) 
+    .then(data => {
+      console.log(data)
+    })
+  }, []);
+
   return (
     <article className="kathai">
       <h1>Kathai</h1>
       <section className="kathai-posts">
-        <Post />
-        <Post />
         <Post />
       </section>
     </article>
