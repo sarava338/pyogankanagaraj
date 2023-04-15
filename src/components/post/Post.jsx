729e1788a6e40a51e176f52854f57ff6ac7d33f1
sarava338 <sarava338@gmail.com>
2023-04-16 02:02:33 +0530

    Add: Added image in Post component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Post = () => {
-  return <div className="post">
-    <h2>post title</h2>
-    <p>post content</p>
-  </div>;
+  return (
+    <section className="post">
+      <img src="image" alt="image" />
+      <div>
+        <h2>post title</h2>
+        <p>
+          post content Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem doloremque maiores modi officiis cum aut nesciunt esse aliquid
+          consequuntur placeat. Illo vel, autem aspernatur aliquid pariatur quis
+          optio odio minima?
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Post;
