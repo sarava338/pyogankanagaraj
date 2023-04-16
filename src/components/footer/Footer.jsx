5f5dea870b10f3d651ce0979bac58f1250ca46ff
sarava338 <sarava338@gmail.com>
2023-04-16 23:31:46 +0530

    Modify: Footer adjusted

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>sarava338</footer>;
+  return (
+    <footer>
+      <hr />
+      <div>
+        <a target="_blank" href="https://sarava338-netlify.app">
+          sarava338
+        </a>{" "}
+        &#169;
+      </div>
+      <hr />
+    </footer>
+  );
 };
 
 export default Footer;
