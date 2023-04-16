73d16e1c542c455a1b596b19643149fb97a6e30c
sarava338 <sarava338@gmail.com>
2023-04-16 17:54:18 +0530

    Add: Footer was added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Kathai from "./pages/Kathai";
 import Kavithai from "./pages/Kavithai";
 import Katturai from "./pages/Katturai";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="kavithai" element={<Kavithai />} />
           <Route path="katturai" element={<Katturai />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
