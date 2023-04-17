bdc1266ad5f86d090fe8dd315d15ba4df3216141
sarava338 <sarava338@gmail.com>
2023-04-17 21:12:05 +0530

    Modify: Modified AddPost Component

--- src/components/post/AddPost.jsx
+++ src/components/post/AddPost.jsx
@@ -7,18 +7,25 @@ import { createPost } from "../../firebase/db";
 const AddPost = ({ col }) => {
   const [newTitle, setNewTitle] = useState("");
   const [newContent, setNewContent] = useState("");
+  const [show, setShow] = useState(false);
 
   const collectionRef = collection(db, col);
 
-  const addPost = () => {
-    console.log(newTitle, newContent);
-    createPost(collectionRef, { title: newTitle, content: newContent });
-    document.getElementById("create-post-title").value = "";
-    document.getElementById("create-post-content").value = "";
+  const addPost = (e) => {
+    e.preventDefault();
+    if (newTitle && newContent) {
+      setShow(false);
+      createPost(collectionRef, { title: newTitle, content: newContent });
+      document.getElementById("create-post-title").value = "";
+      document.getElementById("create-post-content").value = "";
+    } else setShow(true);
   };
 
   return (
-    <section className="create-post">
+    <form className="create-post">
+      <label htmlFor="create-post-title">
+        {col.substring(0, col.length - 3)} title:{" "}
+      </label>
       <input
         onChange={(e) => {
           setNewTitle(e.target.value);
@@ -29,6 +36,9 @@ const AddPost = ({ col }) => {
         className="post-title-input"
         placeholder="enter your title here"
       />
+      <label htmlFor="create-post-content">
+        {col.substring(0, col.length - 3)} content:{" "}
+      </label>
       <textarea
         onChange={(e) => {
           setNewContent(e.target.value);
@@ -39,10 +49,18 @@ const AddPost = ({ col }) => {
         rows="5"
         placeholder="type your content here"
       ></textarea>
-      <button onClick={addPost} className="create-post-button">
+
+      {show && <p className="error">Kindly type something and post</p>}
+
+      <button
+        onClick={(e) => {
+          addPost(e);
+        }}
+        className="create-post-button"
+      >
         Post
       </button>
-    </section>
+    </form>
   );
 };
 
