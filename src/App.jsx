32d6fe86cef6c1adf30b45b0d0f8197907ada1fd
sarava338 <sarava338@gmail.com>
2023-04-22 23:56:11 +0530

    Delete: Deleted App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/header/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
